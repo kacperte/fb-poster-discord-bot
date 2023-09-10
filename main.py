1bf5a33712ddd6b70e00a36fadac979a163bcbb9
Kacper <k.trzepiecinski@gmail.com>
2023-09-10 21:46:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,267 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+from urllib.parse import urljoin
+from scripts import get_credentials, get_from_gcp_storage, get_txt, get_image
+from discord import File
+import io
+
+
+TOKEN = "MTE0NDAyMTE5NTU2NjYxNjY0Ng.GfQoH8.i0yKuwvw_fZhQSK9yXwwbCG5ugsNr09Vu9ot6o"
+GUILD = "HSS Work"
+URL = "http://34.118.109.108/"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+cache = dict()
+
+
+@bot.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+@bot.command()
+async def setLogin(ctx, username: str, password: str):
+    try:
+        grant = get_credentials(URL=URL, login=username.strip(), password=password.strip())
+    except ValueError as e:
+        await ctx.send(str(e))
+        return
+
+    if username in cache:
+        cache[ctx.author]['username'] = username
+        cache[ctx.author]['password'] = password
+        cache[ctx.author]['grant'] = grant
+    else:
+        cache[ctx.author] = {
+            'username': username,
+            'password': password,
+            'grant': grant
+        }
+    await ctx.send(f"Zapisano dane dla {username}.")
+
+
+@bot.command()
+async def getAllMaterial(ctx):
+    user = ctx.author
+
+    if user not in cache:
+        await ctx.send("😢 Nie jesteś zalogowany. Użyj komendy `setLogin` aby się zalogować.")
+        return
+
+    headers = cache[user]['grant']
+
+    full_url = urljoin(URL, "material")
+    try:
+        response_token = requests.get(
+            url=full_url,
+            headers=headers,
+        )
+        response_token.raise_for_status()
+    except requests.HTTPError as e:
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    materials = json.loads(response_token.content)
+
+    material_strings = []
+
+    for material in materials:
+        material_str = (f":file_folder: **Material ID**: {material['id']}\n"
+                        f":bust_in_silhouette: **Klient**: {material['client']}\n"
+                        f":office: **Stanowisko**: {material['position']}\n"
+                        f":frame_photo: **Nazwa grafiki**: {material['image_name']}\n"
+                        f":page_facing_up: **Nazwa copy**: {material['text_name']}\n"
+                        f":pencil: **Autor**: {material['user']['username']}\n")
+        material_strings.append(material_str)
+
+    await ctx.send("\n---\n".join(material_strings))
+
+
+@bot.command()
+async def getMaterial(ctx, id):
+    user = ctx.author
+
+    if user not in cache:
+        await ctx.send("😢 Nie jesteś zalogowany. Użyj komendy `setLogin` aby się zalogować.")
+        return
+
+    headers = cache[user]['grant']
+
+    full_url = urljoin(URL, f"material/{id}")
+    try:
+        response_token = requests.get(
+            url=full_url,
+            headers=headers,
+        )
+        response_token.raise_for_status()
+    except requests.HTTPError as e:
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    material = json.loads(response_token.content)
+
+    raw_image = get_from_gcp_storage(file_name=material['image_name'], bucket_name='fb-poster-bucket')
+    raw_text = get_from_gcp_storage(file_name=material['text_name'], bucket_name='fb-poster-bucket')
+
+    image = get_image(raw_image)
+    text = get_txt(raw_text)
+
+    title = f"**Klient**: {material['client']}\n**Projekt**: {material['position']} \n**Autor**: {material['user']['username']}"
+
+    await ctx.send(title, file=image)
+
+    await ctx.send(f"```{text}```")
+
+
+@bot.command()
+async def createMaterial(ctx, client, position):
+    user = ctx.author
+
+    if len(ctx.message.attachments) != 2:
+        await ctx.send("😢 Dodaj dokładnie dwa pliki: jeden obraz (format .jpg) oraz jeden tekst (format .txt).")
+        return
+
+    txt_like_object = io.BytesIO()
+    image_like_object = io.BytesIO()
+
+    txt_attachment = None
+    image_attachment = None
+
+    for attachment in ctx.message.attachments:
+        if attachment.filename.endswith('.txt'):
+            txt_attachment = attachment
+        elif attachment.filename.endswith('.jpg'):
+            image_attachment = attachment
+
+    if txt_attachment is None or image_attachment is None:
+        await ctx.send(
+            "😢 Dodane przez Ciebie pliki mają zły format. Dodaj obraz (format .jpg) oraz tekst (format .txt).")
+        return
+
+    await txt_attachment.save(txt_like_object)
+    txt_like_object.seek(0)
+
+    await image_attachment.save(image_like_object)
+    image_like_object.seek(0)
+
+    if user not in cache:
+        await ctx.send("😢 Nie jesteś zalogowany. Użyj komendy `setLogin` aby się zalogować.")
+        return
+
+    headers = cache[user]['grant']
+
+    full_url = urljoin(URL, f"material/?client={client}&position={position}")
+
+    try:
+        response_token = requests.post(
+            full_url,
+            files={
+                'image': ('5.jpg', image_like_object.read(), 'image/jpeg'),
+                'text_content': ('5.txt', txt_like_object.read(), 'text/plain')
+            },
+            headers=headers
+)
+
+        response_token.raise_for_status()
+    except requests.HTTPError as e:
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    response_dict = json.loads(response_token.content)
+
+    await ctx.send(f"Nowy materiał o id {response_dict['id']} dodany do bazy. :+1:")
+
+
+@bot.command()
+async def updateMaterial(ctx, id, client, position):
+    user = ctx.author
+
+    if len(ctx.message.attachments) != 2:
+        await ctx.send("😢 Dodaj dokładnie dwa pliki: jeden obraz (format .jpg) oraz jeden tekst (format .txt).")
+        return
+
+    txt_like_object = io.BytesIO()
+    image_like_object = io.BytesIO()
+
+    txt_attachment = None
+    image_attachment = None
+
+    for attachment in ctx.message.attachments:
+        if attachment.filename.endswith('.txt'):
+            txt_attachment = attachment
+        elif attachment.filename.endswith('.jpg'):
+            image_attachment = attachment
+
+    if txt_attachment is None or image_attachment is None:
+        await ctx.send(
+            "😢 Dodane przez Ciebie pliki mają zły format. Dodaj obraz (format .jpg) oraz tekst (format .txt).")
+        return
+
+    await txt_attachment.save(txt_like_object)
+    txt_like_object.seek(0)
+
+    await image_attachment.save(image_like_object)
+    image_like_object.seek(0)
+
+    if user not in cache:
+        await ctx.send("😢 Nie jesteś zalogowany. Użyj komendy `setLogin` aby się zalogować.")
+        return
+
+    headers = cache[user]['grant']
+
+    full_url = urljoin(URL, f"material/{id}?client={client}&position={position}")
+
+    try:
+        response_token = requests.put(
+            full_url,
+            files={
+                'image': ('5.jpg', image_like_object.read(), 'image/jpeg'),
+                'text_content': ('5.txt', txt_like_object.read(), 'text/plain')
+            },
+            headers=headers
+        )
+
+        response_token.raise_for_status()
+    except requests.HTTPError as e:
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    response_dict = json.loads(response_token.content)
+
+    await ctx.send(f"Materiał o id {response_dict['id']} został zaktualizowany. :+1:")
+
+
+@bot.command()
+async def deleteMaterial(ctx, id):
+    user = ctx.author
+
+    if user not in cache:
+        await ctx.send("😢 Nie jesteś zalogowany. Użyj komendy `setLogin` aby się zalogować.")
+        return
+
+    headers = cache[user]['grant']
+
+    full_url = urljoin(URL, f"material/delete/{id}")
+
+    try:
+        response_token = requests.delete(
+            full_url,
+            headers=headers
+        )
+
+        response_token.raise_for_status()
+    except requests.HTTPError as e:
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    response_dict = json.loads(response_token.content)
+
+    await ctx.send(f"Materiał o id {response_dict['id']} został usunięty. :+1:")
+
+bot.run(TOKEN)
---
+++ scripts.py
@@ -0,0 +1,79 @@
+import requests
+import json
+from urllib.parse import urljoin
+import os
+from io import StringIO, BytesIO
+from PIL import Image
+from typing import Union
+from google.cloud import storage
+from discord import File
+from google.oauth2.service_account import Credentials
+
+
+def get_credentials(URL, login, password):
+    full_url = urljoin(URL, "token")
+    try:
+        # Send a request to the token endpoint to authenticate the user
+        response_token = requests.post(
+            url=full_url,
+            data={
+                "grant_type": "password",
+                "username": login,
+                "password": password,
+            },
+        )
+        response_token.raise_for_status()  # Raise an exception if the status code is not in the 200s
+    except requests.HTTPError as e:
+        # If an error occurs, raise an HTTPException with a 400 status code and an error message
+        raise ValueError(f"Invalid credentials, HTTP status code: {e.response.status_code}")
+
+    # Parse the access token from the token response and add it to the headers for subsequent requests
+    response_dict = json.loads(response_token.content)
+    auth_token = response_dict["access_token"]
+    headers = {"Authorization": f"Bearer {auth_token}"}
+
+    return headers
+
+
+def get_from_gcp_storage(file_name: str, bucket_name: str):
+    with open("iam-storage.json") as f:
+        json_content = json.load(f)
+
+    credentials = Credentials.from_service_account_info(json_content)
+
+    storage_client = storage.Client(credentials=credentials)
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    content = blob.download_as_bytes()
+
+    return content
+
+
+def get_image(image: Union[str, bytes]) -> Image:
+    image_obj = None
+    if isinstance(image, str):
+        image_obj = Image.open(image)
+    elif isinstance(image, bytes):
+        image_obj = Image.open(BytesIO(image))
+    else:
+        raise TypeError(f"image must be str or bytes - it's {type(image)}")
+
+    if image_obj:
+        image_binary = BytesIO()
+        image_obj.save(image_binary, 'PNG')
+        image_binary.seek(0)
+        return File(fp=image_binary, filename="image.png")
+
+
+def get_txt(content: Union[bytes, str]) -> str:
+
+    if isinstance(content, bytes):
+        content = content.decode("utf-8")
+    elif not isinstance(content, str):
+        raise TypeError("content must be bytes or str")
+
+    file = StringIO(content)
+
+    return file.read()
+
