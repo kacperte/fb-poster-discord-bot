75553f48956536aa7b833db14248b06c6041d19f
Kacper <k.trzepiecinski@gmail.com>
2023-11-06 23:25:23 +0100

    Add error handling for Discord bot commands
    This commit introduces an error handler for the Discord bot, which catches any errors that occur during the execution of commands. The `on_command_error` event listener is used to send a message back to the user if they enter an invalid command or if an exception is raised during the execution of a command.
    
    The error handler distinguishes between `CommandNotFound` errors, which occur when a user types a non-existent command, and other types of errors, which might be due to incorrect usage or an issue within the command's code. For `CommandNotFound`, a friendly message is sent to the user, prompting them to check the command they entered. For all other errors, a generic error message is sent, and the error is logged for further investigation by the bot maintainers.
    
    Changes include:
    - Adding the `on_command_error` async function to the main bot file.
    - Sending a specific message for `CommandNotFound` errors.
    - Sending a generic error message for all other exceptions.
    - Logging the error details to help with debugging.
    
    This enhancement aims to improve the user experience by providing immediate feedback when a command fails to execute properly.

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ async def on_ready():
         await bot.load_extension(extension)
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Nie znaleziono komendy. Użyj `!help` aby zobaczyć dostępne komendy.')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Brakuje wymaganego argumentu. Użyj `!help <komenda>` aby zobaczyć jak używać tej komendy.')
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send('Nie masz uprawnień do wykonania tej komendy.')
+    else:
+        print(f'Wystąpił nieoczekiwany błąd: {type(error).__name__} - {error}')
+
+
 if __name__ == "__main__":
 
     bot.run(TOKEN)
